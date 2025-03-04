79c81532d7a2bec0471b84ce842d9408c443edf2
Cody <codykohlmann18@gmail.com>
2025-03-03 19:58:21 -0600

    bug fixes

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import flask_SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
-db_name = "database.db"
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
     from .views import views
@@ -18,4 +18,3 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     return app
-
